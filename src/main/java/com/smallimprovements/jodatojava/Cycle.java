fc5ffefa16e9c3185806f01fa489a4bfb3215e2a
Matthew Reid <mreid@small-improvements.com>
2018-12-10 16:51:40 +0100

    convert cycle to java time

--- src/main/java/com/smallimprovements/jodatojava/Cycle.java
+++ src/main/java/com/smallimprovements/jodatojava/Cycle.java
@@ -1,49 +1,49 @@
 package com.smallimprovements.jodatojava;
 
 import lombok.Value;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.Days;
-import org.joda.time.Duration;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 @Value
 public class Cycle {
-    private DateTime createdAt;
-    private DateTime startDate;
+    private Instant createdAt;
+    private Instant startDate;
 
     public boolean isMoreThanTwoDaysOld() {
-        return createdAt.isBefore(new DateTime().minusDays(2));
+        return createdAt.isBefore(Instant.now().minus(2, ChronoUnit.DAYS));
     }
 
     public boolean hasStarted() {
-        return startDate.isBeforeNow();
+        return startDate.isBefore(Instant.now());
     }
 
-    public int daysBetweenFirstJan2018AndCreated() {
-        final DateTime firstJan2018 = new DateTime(2018, 01, 01, 0, 0, 0);
-        return Days.daysBetween(firstJan2018, createdAt).getDays();
+    public long daysBetweenFirstJan2018AndCreated() {
+        final Instant firstJan2018 = LocalDateTime.of(2018, 01, 01, 0, 0, 0).atZone(ZoneId.systemDefault()).toInstant();
+        return Duration.between(firstJan2018, createdAt).toDays();
     }
 
     public Date getStartDateBeginningOfDay() {
-        return startDate.withTimeAtStartOfDay().toDate();
+        return Date.from(startDate.truncatedTo(ChronoUnit.DAYS));
     }
 
     public Date getStartDateAtTimeZone(String timeZone) {
-        return startDate.withZone(DateTimeZone.forID(timeZone)).toDate();
+        return Date.from(startDate.atZone(ZoneId.of(timeZone)).toInstant());
     }
 
     public long daysBetweenCreateAndStartDates() {
-        return new Duration(createdAt, startDate).getStandardDays();
+        return Duration.between(createdAt, startDate).toDays();
     }
 
     public Date lastDayOfPreviousMonth() {
-        return startDate.dayOfMonth()
-                .withMaximumValue()
-                .withTimeAtStartOfDay()
-                .minusMonths(1)
-                .toDate();
+        return Date.from(startDate
+                .truncatedTo(ChronoUnit.DAYS)
+                .atZone(ZoneId.systemDefault())
+                .minus(1, ChronoUnit.MONTHS).toInstant());
     }
 
 
